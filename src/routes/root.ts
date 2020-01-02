5a442608d51d7f4af16008ef078aa7e514020efc
Nikita Moiseev <moiseev.nikita@gmail.com>
2020-01-03 01:11:38 +0300

    added 404

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express, { Response } from 'express'
 
 import apiRouter from './api'
 
 const router = express.Router()
 
 router.use('/api', apiRouter)
+
+router.use((_, res: Response) => res.status(404).json({ message: '404 - Not found' }))
 
 export default router
